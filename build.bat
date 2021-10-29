0eaf68d364b6ddf9092cc67f7ddf52e67d0c438e
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2021-10-29 22:01:33 +0200

    Small changes

--- build.bat
+++ build.bat
@@ -3,14 +3,14 @@ if not exist "build" mkdir "build"
 pushd "build"
 
 @REM MSVC
-set "compiler_flags=-Oi -O2 -Zi -FC -WX -W4 -wd4201 -wd4189 -wd4100 -GR- -EHsc -MT -nologo -D_CRT_SECURE_NO_WARNINGS"
-set "files=../src/threads.cpp ../src/main.cpp ../src/loader.cpp ../src/intersect.cpp ../src/vec.cpp ../src/mat4.cpp ../src/math.cpp ../src/material.cpp ../src/mat3.cpp"
-cl %compiler_flags% %files% /link -opt:ref user32.lib gdi32.lib winmm.lib /out:pathtracer.exe
+@REM set "compiler_flags=-Oi -O2 -Zi -FC -WX -W4 -wd4201 -wd4189 -wd4100 -GR- -EHsc -MT -nologo -D_CRT_SECURE_NO_WARNINGS"
+@REM set "files=../src/threads.cpp ../src/main.cpp ../src/loader.cpp ../src/intersect.cpp ../src/vec.cpp ../src/mat4.cpp ../src/math.cpp ../src/material.cpp ../src/mat3.cpp"
+@REM cl %compiler_flags% %files% /link -opt:ref user32.lib gdi32.lib winmm.lib /out:pathtracer.exe
 
 @REM Clang
-@REM set "compiler_flags=-O2 -g3 -Wall -Wextra -Werror -Wno-missing-braces -Wno-unused-variable -Wno-missing-field-initializers -D_CRT_SECURE_NO_WARNINGS"
-@REM set "files=../src/threads.cpp ../src/main.cpp ../src/loader.cpp ../src/intersect.cpp ../src/vec.cpp ../src/mat4.cpp ../src/math.cpp ../src/material.cpp"
-@REM clang++ %compiler_flags% %files% -o pathtracer.exe
+set "compiler_flags=-O2 -g3 -Wall -Wextra -Werror -Wno-missing-braces -Wno-unused-variable -Wno-missing-field-initializers -D_CRT_SECURE_NO_WARNINGS"
+set "files=../src/threads.cpp ../src/main.cpp ../src/loader.cpp ../src/intersect.cpp ../src/vec.cpp ../src/mat4.cpp ../src/mat3.cpp ../src/math.cpp ../src/material.cpp"
+clang -ftime-trace %compiler_flags% %files% -o pathtracer.exe
 
 popd
 
