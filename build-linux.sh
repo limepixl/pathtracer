2a226c3ea6fc78c5cfc1949bf327c3fadd6a841b
Stefan Ivanovski <limepixl.dev@protonmail.com>
2022-04-06 22:58:32 +0200

    Tiny changes to Linux build

--- build-linux.sh
+++ build-linux.sh
@@ -4,4 +4,5 @@
 cd ./build-linux
 
 # cmake -S ../ -B . -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+# cmake -S ../ -B . -G "Ninja" -DCMAKE_BUILD_TYPE=RELEASE
 ninja
