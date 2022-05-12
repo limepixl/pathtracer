44f23a8c435bc90e28b6722b48594ec143430aa1
Stefan Ivanovski <limepixl.dev@protonmail.com>
2022-05-12 18:59:33 +0200

    Naming convention in Linux files

--- src/threads_linux.cpp
+++ src/threads_linux.cpp
@@ -7,26 +7,26 @@
 
 void *render_function(void *param)
 {
-	RenderData *renderData = (RenderData *)param;
+	RenderData *render_data = (RenderData *)param;
 
-	uint32 width = renderData->width;
-	uint32 height = renderData->height;
+	uint32 width = render_data->width;
+	uint32 height = render_data->height;
 
-	Vec3f gridOrigin = renderData->gridOrigin;
-	Vec3f gridX = renderData->gridX;
-	Vec3f gridY = renderData->gridY;
-	Vec3f eye = renderData->eye;
+	Vec3f grid_origin = render_data->grid_origin;
+	Vec3f grid_x = render_data->grid_x;
+	Vec3f grid_y = render_data->grid_y;
+	Vec3f eye = render_data->eye;
 
-	Scene scene = renderData->scene;
+	Scene scene = render_data->scene;
 
-	uint8 *memory = (uint8 *)renderData->threadMemoryChunk;
+	uint8 *memory = (uint8 *)render_data->thread_memory_chunk;
 
 	// Margins
-	uint32 startX = renderData->startX;
-	uint32 endX = renderData->endX;
+	uint32 start_x = render_data->start_x;
+	uint32 end_x = render_data->end_x;
 
-	uint32 startY = renderData->startY;
-	uint32 endY = renderData->endY;
+	uint32 start_y = render_data->start_y;
+	uint32 end_y = render_data->end_y;
 
 	// Initialize a PCG context for each thread
 	pcg32_random_t rng {};
@@ -34,29 +34,29 @@ void *render_function(void *param)
 
 	// Index for memory buffer
 	uint32 index = 0;
-	for (uint32 ypixel = renderData->startY; ypixel < endY; ypixel++)
+	for (uint32 ypixel = render_data->start_y; ypixel < end_y; ypixel++)
 	{
-		uint32 y = (ypixel - renderData->startY);
+		uint32 y = (ypixel - render_data->start_y);
 
-		for (uint32 xpixel = renderData->startX; xpixel < endX; xpixel++)
+		for (uint32 xpixel = render_data->start_x; xpixel < end_x; xpixel++)
 		{
-			uint32 x = (xpixel - renderData->startX);
+			uint32 x = (xpixel - render_data->start_x);
 
 			Vec3f color = { 0.0f, 0.0f, 0.0f };
 
 			for (int16 sample = 0; sample < NUM_SAMPLES; sample++)
 			{
 				// TODO: verify that this math checks out
-				Vec2f offsetToPixelCenter = { 0.5f, 0.5f };
-				Vec2f uvOffset = RandomVec2fPCG(&rng) - offsetToPixelCenter;
+				Vec2f offset_to_pixel_center = { 0.5f, 0.5f };
+				Vec2f uv_offset = RandomVec2fPCG(&rng) - offset_to_pixel_center;
 
-				float u = ((float)xpixel + uvOffset.x) / (float)width;
-				float v = ((float)ypixel + uvOffset.y) / (float)height;
+				float u = ((float)xpixel + uv_offset.x) / (float)width;
+				float v = ((float)ypixel + uv_offset.y) / (float)height;
 
-				Vec3f pointOnGrid = gridOrigin + u * gridX + v * gridY;
-				Vec3f rayDirection = NormalizeVec3f(pointOnGrid - eye);
+				Vec3f point_on_grid = grid_origin + u * grid_x + v * grid_y;
+				Vec3f ray_direction = NormalizeVec3f(point_on_grid - eye);
 
-				Ray ray = { eye, rayDirection, {} };
+				Ray ray = { eye, ray_direction, {} };
 				// color += EstimatorPathTracingLambertian(ray, scene, &rng);
 				// color += EstimatorPathTracingLambertianNEE(ray, scene, &rng);
 				color += EstimatorPathTracingMIS(ray, scene, &rng);
@@ -101,21 +101,21 @@ void *CreateThreadLinux(void *param)
 	}
 	*/
 
-	pthread_t *threadHandle = (pthread_t *)malloc(sizeof(pthread_t));
-	if (pthread_create(threadHandle, NULL, &render_function, param))
+	pthread_t *thread_handle = (pthread_t *)malloc(sizeof(pthread_t));
+	if (pthread_create(thread_handle, NULL, &render_function, param))
 	{
 		printf("Failed to create pthread_t!\n");
 		return NULL;
 	}
 
 	// printf("Successfully created thread with ID: %d\n", (int)threadID);
-	return threadHandle;
+	return thread_handle;
 }
 
-void WaitForThreadLinux(void *threadHandle)
+void WaitForThreadLinux(void *thread_handle)
 {
-	pthread_join(*(pthread_t *)threadHandle, NULL);
-	free(threadHandle);
+	pthread_join(*(pthread_t *)thread_handle, NULL);
+	free(thread_handle);
 }
 
 bool CanThreadStartLinux(void *handle)
