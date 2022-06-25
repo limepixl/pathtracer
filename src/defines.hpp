112c3acb956fa9627e0e66678a5c8fa634799216
Stefan Ivanovski <sivanovski.dev@gmail.com>
2022-06-25 17:08:32 +0200

    Small tweaks

--- src/defines.hpp
+++ src/defines.hpp
@@ -1,28 +1,5 @@
 #pragma once
 
-#define NUM_THREADS 11
-
-#define WIDTH 500
-#define HEIGHT 500
-
-#define BOUNCE_COUNT 5
-#define NUM_SAMPLES 100
-#define NUM_SHADOW_RAYS 1
-
-#define BVH_NUM_LEAF_TRIS 4
-
-#define TMIN 0.001f
-#define TMAX 10000.0f
-#define PI 3.14159265f
-#define EPSILON 0.000001f
-#define ENVIRONMENT_MAP_LE 1.0f
-
-#define TWO_SIDED_LIGHT 0
-
-#define IDEAL_SPECULAR_TINT 1.0f
-
-#define PREDICTABLE_RAND 0
-
 // NOTE: This only holds for LLP64
 typedef unsigned char uint8;
 typedef unsigned short uint16;
@@ -33,12 +10,36 @@ typedef char int8;
 typedef short int16;
 typedef int int32;
 typedef long int64;
+// NOTE: This only holds for LLP64
+
+constexpr uint8 NUM_THREADS = 11;
+
+constexpr uint32 WIDTH = 500;
+constexpr uint32 HEIGHT = 500;
+
+constexpr uint8 BOUNCE_COUNT = 5;
+constexpr uint16 NUM_SAMPLES = 100;
+constexpr uint8 NUM_SHADOW_RAYS = 1;
+
+constexpr uint8 BVH_NUM_LEAF_TRIS = 4;
+
+constexpr float TMIN = 0.001f;
+constexpr float TMAX = 10000.0f;
+constexpr float PI = 3.14159265f;
+constexpr float EPSILON = 0.000001f;
+constexpr float ENVIRONMENT_MAP_LE = 1.0f;
+
+constexpr bool TWO_SIDED_LIGHT = false;
+
+constexpr float IDEAL_SPECULAR_TINT = 1.0f;
+
+constexpr bool PREDICTABLE_RAND = false;
 
 // Macro to return stack allocated array length
 #define ARRAYCOUNT(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 // a == b is transformed into Abs(a-b) <= FLOAT_EQUALITY_PRECISION
-#define FLOAT_EQUALITY_PRECISION EPSILON
+constexpr float FLOAT_EQUALITY_PRECISION = EPSILON;
 
 // ASSERT writes to 0x0, which will throw an access
 // violation exception. ASSERT is used when we want
