3c3cda2d5662b4c38d12d6e1b2b3def1d7e5d642
Stefan Ivanovski <sivanovski.dev@gmail.com>
2022-10-13 13:26:31 +0200

    NEE estimator works with specular BRDF!

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 	// Set up data to be passed to SSBOs
 
 	Array<MaterialGLSL> materials_ssbo;
-	materials_ssbo.append(MaterialGLSL(Vec3f(0.0f), Vec3f(0.0f), Vec3f(10.0f), 0.0f, 0.0f, MaterialType::MATERIAL_LAMBERTIAN));
+	materials_ssbo.append(MaterialGLSL(Vec3f(0.0f), Vec3f(0.0f), Vec3f(100.0f), 0.0f, 0.0f, MaterialType::MATERIAL_LAMBERTIAN));
 	materials_ssbo.append(MaterialGLSL(Vec3f(0.0f), Vec3f(0.944f, 0.776f, 0.373f), Vec3f(0.0f), 0.0f, 0.0f, MaterialType::MATERIAL_SPECULAR));
 	materials_ssbo.append(MaterialGLSL(Vec3f(0.0f), Vec3f(0.944f, 0.776f, 0.373f), Vec3f(0.0f), 0.025f, 0.0f, MaterialType::MATERIAL_SPECULAR));
 	materials_ssbo.append(MaterialGLSL(Vec3f(0.0f), Vec3f(0.944f, 0.776f, 0.373f), Vec3f(0.0f), 0.075f, 0.0f, MaterialType::MATERIAL_SPECULAR));
@@ -93,18 +93,18 @@ int main(int argc, char *argv[])
 	Array<SphereGLSL> spheres_ssbo;
 
 	// Grid of spheres
-	spheres_ssbo.append(SphereGLSL(Vec3f(-0.99f,  0.33f, -4.0f), 0.33f, 1));
-	spheres_ssbo.append(SphereGLSL(Vec3f(-0.33f,  0.33f, -4.0f), 0.33f, 2));
-	spheres_ssbo.append(SphereGLSL(Vec3f( 0.33f,  0.33f, -4.0f), 0.33f, 3));
-	spheres_ssbo.append(SphereGLSL(Vec3f( 0.99f,  0.33f, -4.0f), 0.33f, 4));
-	spheres_ssbo.append(SphereGLSL(Vec3f(-0.99f, -0.33f, -4.0f), 0.33f, 5));
-	spheres_ssbo.append(SphereGLSL(Vec3f(-0.33f, -0.33f, -4.0f), 0.33f, 6));
-	spheres_ssbo.append(SphereGLSL(Vec3f( 0.33f, -0.33f, -4.0f), 0.33f, 7));
-	spheres_ssbo.append(SphereGLSL(Vec3f( 0.99f, -0.33f, -4.0f), 0.33f, 8));
+	spheres_ssbo.append(SphereGLSL(Vec3f(-0.99f,  0.33f, -3.0f), 0.33f, 1));
+	spheres_ssbo.append(SphereGLSL(Vec3f(-0.33f,  0.33f, -3.0f), 0.33f, 2));
+	spheres_ssbo.append(SphereGLSL(Vec3f( 0.33f,  0.33f, -3.0f), 0.33f, 3));
+	spheres_ssbo.append(SphereGLSL(Vec3f( 0.99f,  0.33f, -3.0f), 0.33f, 4));
+	spheres_ssbo.append(SphereGLSL(Vec3f(-0.99f, -0.33f, -3.0f), 0.33f, 5));
+	spheres_ssbo.append(SphereGLSL(Vec3f(-0.33f, -0.33f, -3.0f), 0.33f, 6));
+	spheres_ssbo.append(SphereGLSL(Vec3f( 0.33f, -0.33f, -3.0f), 0.33f, 7));
+	spheres_ssbo.append(SphereGLSL(Vec3f( 0.99f, -0.33f, -3.0f), 0.33f, 8));
 
 	// Point light and sphere next to it
-	spheres_ssbo.append(SphereGLSL(Vec3f(0.0f, 1.0f, -4.0f), 0.3f, 9));
-	spheres_ssbo.append(SphereGLSL(Vec3f(0.0f, 0.0f, 1.0f), 1.0f, 0));
+	spheres_ssbo.append(SphereGLSL(Vec3f(0.0f, 1.0f, -3.0f), 0.3f, 9));
+	spheres_ssbo.append(SphereGLSL(Vec3f(0.0f, 0.0f, 1.0f), 0.3f, 0));
 
 #if 0
 	Array<TriangleGLSL> model_tris_ssbo(tris.size);
