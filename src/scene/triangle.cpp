6206719e0186747b830afd786b6e3948ec1cf245
Stefan Ivanovski <sivanovski.dev@gmail.com>
2023-05-16 21:19:21 +0200

    Added support for separate vertex normals and updated constructors for both Triangle versions

--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -3,7 +3,8 @@
 #include <cmath>
 
 Triangle::Triangle()
-        : v0(0.0f), v1(0.0f), v2(0.0f), normal(0.0f),
+        : v0(0.0f), v1(0.0f), v2(0.0f),
+	      n0(0.0f), n1(0.0f), n2(0.0f),
           uv0(0.0f), uv1(0.0f), uv2(0.0f),
           edge1(0.0f), edge2(0.0f), mat_index(0) {}
 
@@ -15,21 +16,23 @@ Triangle::Triangle(Vec3f v0, Vec3f v1, Vec3f v2, uint32 mat_index)
 {
     Vec3f A = v1 - v0;
     Vec3f B = v2 - v0;
-    normal = pixl::normalize(pixl::cross(A, B));
+    n0 = pixl::normalize(pixl::cross(A, B));
+	n1 = n0;
+	n2 = n0;
 }
 
-Triangle::Triangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f normal, uint32 mat_index)
-        : v0(v0), v1(v1), v2(v2), normal(normal),
+Triangle::Triangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f n0, Vec3f n1, Vec3f n2, uint32 mat_index)
+        : v0(v0), v1(v1), v2(v2),
+	      n0(n0), n1(n1), n2(n2),
           uv0(0.0f), uv1(0.0f), uv2(0.0f),
           mat_index(mat_index) {}
 
 Triangle::Triangle(Array<Vec3f> &vertices, Array<Vec3f> &normals, Array<Vec2f> &tex_coords, uint32 mat_index)
         : v0(vertices[0]), v1(vertices[1]), v2(vertices[2]),
+	      n0(normals[0]), n1(normals[1]), n2(normals[2]),
           uv0(tex_coords[0]), uv1(tex_coords[1]), uv2(tex_coords[2]),
           mat_index(mat_index)
-{
-    normal = pixl::normalize(normals[0] + normals[1] + normals[2]);
-}
+{}
 
 float Area(Triangle *tri)
 {
@@ -89,10 +92,22 @@ Vec3f TriangleGLSL::v2() const
 	return {data3.x, data3.y, data3.z};
 }
 
-TriangleGLSL::TriangleGLSL(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2, uint32 mat_index, const Vec2f &uv0, const Vec2f &uv1, const Vec2f &uv2)
+TriangleGLSL::TriangleGLSL(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2,
+			 const Vec2f &uv0, const Vec2f &uv1, const Vec2f &uv2,
+			 const Vec3f &n0, const Vec3f &n1, const Vec3f &n2,
+			 uint32 mat_index)
 	: data1(v0.x, v0.y, v0.z, (float)mat_index),
-	  data2(v1.x, v1.y, v1.z, 0.0f),
-	  data3(v2.x, v2.y, v2.z, 0.0f),
-	  data4(uv0.x, uv0.y, uv1.x, uv1.y),
-	  data5(uv2.x, uv2.y, 0.0f, 0.0f)
+	  data2(v1.x, v1.y, v1.z, uv0.x),
+	  data3(v2.x, v2.y, v2.z, uv0.y),
+	  data4(uv1.x, uv1.y, uv2.x, uv2.y),
+	  data5(n0.x, n0.y, n0.z, n1.x),
+	  data6(n1.y, n1.z, n2.x, n2.y),
+	  data7(n2.z, 0.0f, 0.0f, 0.0f)
+{}
+
+TriangleGLSL::TriangleGLSL(const Triangle &triangle)
+	: TriangleGLSL(triangle.v0, triangle.v1, triangle.v2,
+				   triangle.uv0, triangle.uv1, triangle.uv2,
+				   triangle.n0, triangle.n1, triangle.n2,
+				   triangle.mat_index)
 {}
--- src/scene/triangle.hpp
+++ src/scene/triangle.hpp
@@ -6,7 +6,7 @@
 struct Triangle
 {
     Vec3f v0, v1, v2;
-    Vec3f normal;
+    Vec3f n0, n1, n2;
     Vec2f uv0, uv1, uv2;
 
     // Precomputed values
@@ -18,7 +18,7 @@ struct Triangle
 
     Triangle(Vec3f v0, Vec3f v1, Vec3f v2, uint32 mat_index);
 
-    Triangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f normal, uint32 mat_index);
+    Triangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f n0, Vec3f n1, Vec3f n2, uint32 mat_index);
 
     Triangle(Array<Vec3f> &vertices, Array<Vec3f> &normals, Array<Vec2f> &tex_coords, uint32 mat_index);
 };
@@ -32,13 +32,19 @@ void ApplyTranslationToTriangle(Triangle *tri, Vec3f translation_vec);
 struct TriangleGLSL
 {
     Vec4f data1; // v0.x, v0.y, v0.z, mat_index
-    Vec4f data2; // v1.x, v1.y, v1.z, 0
-    Vec4f data3; // v2.x, v2.y, v2.z, 0
-    Vec4f data4; // uv0.x, uv0.y, uv1.x, uv1.y
-    Vec4f data5; // uv2.x, uv2.y, 0, 0
+    Vec4f data2; // v1.x, v1.y, v1.z, uv0.x
+    Vec4f data3; // v2.x, v2.y, v2.z, uv0.y
+    Vec4f data4; // uv1.x, uv1.y, uv2.x, uv2.y
+	Vec4f data5; // n0.x, n0.y, n0.z, n1.x
+	Vec4f data6; // n1.y, n1.z, n2.x, n2.y
+	Vec4f data7; // n2.z, 0, 0, 0
 
 	TriangleGLSL() = default;
-	TriangleGLSL(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2, uint32 mat_index, const Vec2f &uv0, const Vec2f &uv1, const Vec2f &uv2);
+	TriangleGLSL(const Triangle &triangle);
+	TriangleGLSL(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2,
+				 const Vec2f &uv0, const Vec2f &uv1, const Vec2f &uv2,
+				 const Vec3f &n0, const Vec3f &n1, const Vec3f &n2,
+				 uint32 mat_index);
 
 	[[nodiscard]] Vec3f v0() const;
 	[[nodiscard]] Vec3f v1() const;
